7223e4e3fa56683127eb210c3773b03a2d8bc446
Bri <jreanah@gmail.com>
2026-07-13 10:36:59 -0400

    Fixed csv example

--- hands-free/sw.js
+++ hands-free/sw.js
@@ -9,7 +9,7 @@
 //
 // Bump CACHE_VERSION on every deploy to purge stale assets.
 
-const CACHE_VERSION = 'v20260713143427';
+const CACHE_VERSION = 'v20260713143659';
 
 const APP_SHELL = [
   './',
